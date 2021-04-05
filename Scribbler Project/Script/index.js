47d9d2c65426f0b3245cf5a7d88678c268335037
Gajanan Ambore <75257341+tyson1992@users.noreply.github.com>
2021-04-05 23:23:18 +0530

    Add files via upload

---
+++ Scribbler Project/Script/index.js
@@ -0,0 +1,37 @@
+// JavaScript source code
+
+
+function signup(){
+
+    document.getElementById("modal").style.display = "block";
+
+ 
+
+}
+
+function signup1() {
+    document.getElementById("modal").style.display = "none";
+
+}
+
+function signin() {
+
+    document.getElementById("modal2").style.display = "block";
+
+}
+
+function signin1() {
+    document.getElementById("modal2").style.display = "none";
+
+}
+
+function createpost() {
+
+    document.getElementById("modal3").style.display = "block";
+
+}
+function creatpostclose() {
+
+    document.getElementById("modal3").style.display = "none";
+
+}
