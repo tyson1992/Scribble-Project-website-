7b1d71e71269a8b63d2f07e0e26a6ac5ebaaf3e4
Tyson <75257341+tyson1992@users.noreply.github.com>
2021-04-10 15:14:52 +0530

    updated version

---
+++ Script/postslist.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+function posterno() {
+
+    document.getElementById("box1").style.display = "block";
+
+
+}
+
+function posterno1() {
+
+    document.getElementById("box1").style.display = "none";
+}
+
+function removeele() {
+
+
+    var elem = document.getElementById("post1");
+
+   
+        elem.parentNode.removeChild(elem);
+        
+}
+
+
